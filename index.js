49b32f0c55ebf0df567ed18d4cf52fa84f18cf9f
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-27 11:35:56 +0600

    item collection added

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+// MonoDB Connect
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k6zwazt.mongodb.net/?retryWrites=true&w=majority`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,6 +25,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const itemCollection = client.db("medicalCamp").collection("item"); 
+
+
+    // Item Collection
+
+    app.get("/item", async (req, res) => {
+        const result = await itemCollection.find().toArray();
+        res.send(result);
+      });
+
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
